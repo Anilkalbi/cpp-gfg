5569e183ec9af9313b88ae16dc60d33492eb7047
Anilkalbi <141136121+Anilkalbi@users.noreply.github.com>
2024-02-01 11:41:36 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter the number:";
+    cin>>a;
+    if(a>=10)
+    cout<<"its true:";
+    else
+    cout<<"its false:";
+    return 0;
+}
