1c785ffa70bf1eae64ea36f371695399e032236b
Anilkalbi <141136121+Anilkalbi@users.noreply.github.com>
2024-02-01 11:42:19 +0530

    Add files via upload

---
+++ elseif.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter the number:";
+    cin>>a;
+    if(a>0)
+    {
+        cout<<"statment 1 is true:";
+    }
+     else if(a<0)
+     {
+        cout<<"statement 2 is true:";
+     }
+     else
+     {
+     cout<<"its zero:";
+     }
+
+
+}
