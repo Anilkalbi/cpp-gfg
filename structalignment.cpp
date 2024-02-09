412fdb8fb968eba7ea8ac9b32dffbf2bcd4a2908
Anilkalbi <141136121+Anilkalbi@users.noreply.github.com>
2024-02-09 18:00:32 +0530

    Add files via upload

---
+++ structalignment.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+struct s1
+{
+    char c1;
+    double d1;
+    char c2;
+};
+struct s2
+{
+    char c1;
+    string c2;
+    double d1;
+};
+int main()
+{
+    cout<<sizeof(s1)<<" "<<sizeof(s2);
+    }
