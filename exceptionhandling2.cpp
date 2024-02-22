b89a9e99197704a07657d382566ec705becaf53e
Anilkalbi <141136121+Anilkalbi@users.noreply.github.com>
2024-02-22 08:51:42 +0530

    Add files via upload

---
+++ exceptionhandling2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double x,y;
+    cin>>x>>y;
+    try{
+        if(x==0.0)
+        throw 0;
+        if(y==0.0)
+        throw string("y is zero");
+        if(x+y<0.0)
+        throw (x+y);
+    }
+    catch(int e1)
+    {
+        cout<<e1;
+    }
+    catch(string &e2)
+    {
+        cout<<e2;
+    }
+    catch(...)
+    {
+        cout<<"x+y is less than 0";
+    }
+    cout<<"\nbye";
+    return 0;
+}
