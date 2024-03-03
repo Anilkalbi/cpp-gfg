6647d581d99997310736ab258ef63a0353ba2ebe
Anilkalbi <141136121+Anilkalbi@users.noreply.github.com>
2024-03-03 22:13:16 +0530

    Add files via upload

---
+++ lambda3.cpp
@@ -0,0 +1,11 @@
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    vector<int> v={10,20,30};
+    for_each(v.begin(),v.end(),[](int &x){x = x*2;});
+    for_each(v.begin(),v.end(),[](int x){cout<<x<<" ";});
+    return 0;
+}
