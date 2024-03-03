c141a742c9472f5c9dd9bce15124cefd8a037db7
Anilkalbi <141136121+Anilkalbi@users.noreply.github.com>
2024-03-03 22:12:51 +0530

    Add files via upload

---
+++ lambdaex2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int arr[]={-20,10,-30,5};
+    auto mycmp=[](int a,int b)
+    {
+        return abs(a)<abs(b);
+    };
+    sort(arr,arr+n,mycmp);
+    for(auto x,arr)
+    cout<<x<<" ";
+    return 0;
+}
