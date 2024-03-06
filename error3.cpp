36056c7a16e277c000a9c506938ab81ac2c23f06
Anilkalbi <141136121+Anilkalbi@users.noreply.github.com>
2024-03-06 23:11:18 +0530

    Add files via upload

---
+++ error3.cpp
@@ -0,0 +1,11 @@
+// C++ program to illustrate
+// linker error
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void Main() // Here Main() should be main()
+{
+    int a = 10;
+    cout << " "<< a; 
+}
+ 
