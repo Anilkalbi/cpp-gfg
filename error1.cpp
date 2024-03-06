202bb2df98b4c81969307c1df49612f5c9479c6e
Anilkalbi <141136121+Anilkalbi@users.noreply.github.com>
2024-03-06 23:09:04 +0530

    Add files via upload

---
+++ error1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+ 
+int main(void)
+{
+    // while() cannot contain "." as an argument.
+    while(.) 
+    {
+        cout <<"hello";
+    }
+    return 0;
+}
